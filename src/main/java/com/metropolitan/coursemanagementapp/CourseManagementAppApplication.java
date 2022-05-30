1fdabe568fe3acdc4ecd96f5058e3e0259f811c6
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-05-30 02:48:56 +0200

    initial commit

---
+++ src/main/java/com/metropolitan/coursemanagementapp/CourseManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.metropolitan.coursemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metropolitan/coursemanagementapp/CourseManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.coursemanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
