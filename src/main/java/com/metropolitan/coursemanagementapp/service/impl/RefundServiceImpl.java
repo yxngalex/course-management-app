096fbf8801adb968f74b4b277742da67c4efc888
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-06-07 01:12:59 +0200

    finished CRUD for role and refund

--- src/main/java/com/metropolitan/coursemanagementapp/service/impl/RefundServiceImpl.java
+++ src/main/java/com/metropolitan/coursemanagementapp/service/impl/RefundServiceImpl.java
@@ -1,8 +1,43 @@
 package com.metropolitan.coursemanagementapp.service.impl;
 
+import com.metropolitan.coursemanagementapp.entity.Course;
+import com.metropolitan.coursemanagementapp.entity.Refund;
+import com.metropolitan.coursemanagementapp.repository.RefundRepository;
 import com.metropolitan.coursemanagementapp.service.RefundService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
+@RequiredArgsConstructor
 public class RefundServiceImpl implements RefundService {
+    private final RefundRepository refundRepository;
+
+    @Override
+    public List<Refund> getAllRefunds() {
+        return refundRepository.findAll();
+    }
+
+    @Override
+    public Refund getRefundById(Integer refundId) {
+        return refundRepository.findById(refundId)
+                .orElseThrow(() -> new NoSuchElementException("RefundService.notFound"));
+    }
+
+    @Override
+    public Refund saveRefund(Refund refund) {
+        return null;
+    }
+
+    @Override
+    public Refund updateRefund(Refund refund) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer refundId) {
+
+    }
 }
