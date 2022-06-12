7c352dcf0c5ae5deaa2853383a0b889c8d3b1771
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-06-13 01:50:38 +0200

    added request mapping to user

--- src/main/java/com/metropolitan/coursemanagementapp/controller/UserController.java
+++ src/main/java/com/metropolitan/coursemanagementapp/controller/UserController.java
@@ -5,14 +5,12 @@ import com.metropolitan.coursemanagementapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
