2ec72e3c3e9be5fc090b9badc69fa7868bf43d2b
stefan-gg <stefan.gogic01@gmail.com>
2022-06-15 13:01:05 +0200

    Course CRUD added, default path /course

--- src/main/java/com/metropolitan/coursemanagementapp/controller/CourseController.java
+++ src/main/java/com/metropolitan/coursemanagementapp/controller/CourseController.java
@@ -1,11 +1,15 @@
 package com.metropolitan.coursemanagementapp.controller;
 
+import com.metropolitan.coursemanagementapp.entity.Category;
 import com.metropolitan.coursemanagementapp.entity.Course;
+import com.metropolitan.coursemanagementapp.service.CategoryService;
 import com.metropolitan.coursemanagementapp.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class CourseController {
 
     private final CourseService courseService;
+    private final CategoryService categoryService;
 
     @GetMapping("/")
     public String getAllCourses(Model model){
@@ -23,20 +28,23 @@ public class CourseController {
     @GetMapping("/show_new_course_form")
     public String showNewUserForm(Model model){
         Course course = new Course();
+        List<Category> categoryList = categoryService.getAllCategories();
+
         model.addAttribute("course", course);
+        model.addAttribute("categoryList", categoryList);
         return "author/new_course";
     }
 
     @PostMapping(value = "/delete_course/{id}")
     public String deleteCourse(@PathVariable Integer id){
         courseService.deleteById(id);
-        return "redirect:/new_course";
+        return "redirect:/course/";
     }
 
     @PostMapping("/save_course")
     public String saveCourse(@ModelAttribute("course") Course course){
         courseService.saveCourse(course);
-        return "redirect:/new_course";
+        return "redirect:/course/";
     }
 
     @GetMapping("/show_update_course_form/{id}")
