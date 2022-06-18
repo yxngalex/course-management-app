cfc414ded78e025d3dd80f288988ecaee9dd70c9
stefan-gg <stefan.gogic01@gmail.com>
2022-06-18 16:56:54 +0200

    Project finished, only tests left.

--- src/main/java/com/metropolitan/coursemanagementapp/controller/OrderDetailsController.java
+++ src/main/java/com/metropolitan/coursemanagementapp/controller/OrderDetailsController.java
@@ -1,5 +1,6 @@
 package com.metropolitan.coursemanagementapp.controller;
 
+import com.metropolitan.coursemanagementapp.entity.OrderDetails;
 import com.metropolitan.coursemanagementapp.service.OrderDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/order_details")
@@ -16,7 +20,8 @@ public class OrderDetailsController {
 
     @GetMapping()
     public String getAllOrderDetails(Model model) {
-        model.addAttribute("getAllOrderDetails", orderDetailsService.getAllOrderDetails());
+        List<OrderDetails> orderDetailsList = orderDetailsService.getAllOrderDetails();
+        model.addAttribute("getAllOrderDetails", orderDetailsList.get(orderDetailsList.size() - 1));
         return "user/order";
     }
 
